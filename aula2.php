1174b17635e4f4419943b02d6419432e4cdb29ce
Aluno <gustavooguidetti@gmail.com>
2020-03-02 09:30:17 -0300

    vestebroca

---
+++ aula2.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <style>
+    .noticia1  h2 {
+      background-color:#e6e6ff
+    }
+
+    .noticia1  a {
+      color:#000033
+    }
+    </style>
+    <div class="noticia1">
+<h2><a href="https://www.espn.com.br/nba/jogador/_/id/3468/russell-westbrook">Westbrook</a> atinge a marca dos 41 pontos na noite deste sabado</h2>
+      <img src="https://cdn.vox-cdn.com/thumbor/6GpAEH4-VYUndZE4cl3pG22e5vY=/0x0:4860x3240/1200x800/filters:focal(1882x294:2658x1070)/cdn.vox-cdn.com/uploads/chorus_image/image/66411295/1209582030.jpg.0.jpg" width="630" height="400" alt="">
+
+    </div>
+
+
+    <div class="noticia1">
+
+      <h4>O Armador foi o maior pontuador dos Rockets no jogo em Boston</h4>
+      <table border ='1'>
+        <tr>
+        <td>Russell Westbrook</td>
+        <td>41</td>
+        </tr>
+        <tr><td>James Harden </td>
+        <td>21</td></tr>
+        <tr><td>PJ Tucker</td>
+        <td>9</td>
+        </tr>
+        <tr>
+        <td>House Jr.</td>
+        <td>9</td>
+        </tr>
+        </table>
+
+    </div>
+
+
+    <div class="noticia1">
+
+      <p>   Com grande atuação de Russell Westbrook, o Houston Rockets venceu o Boston Celtics na noite deste sábado,<br>
+         em mais uma rodada da NBA. O time de Boston forçou a prorrogação graças a uma cesta de três no apagar das luzes<br>
+         do último quarto, mas o armador dos Rockets ditou o ritmo da equipe e garantiu o placar apertado de 111 a 110.</p>
+        <p>   Westbrook foi o cestinha da partida com 41 pontos, além de oito rebotes e cinco assistências. Pelo lado dos Celtics,<br>
+           Jayson Tatum foi o maior pontuador, com 32 pontos - destaque também para Daniel Theis, que conseguiu 15 rebotes durante o confronto.</p>
+
+
+    </div>
+
+  </body>
+</html>
