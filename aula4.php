ec2c9390ca295c9acc599e9bf4c61179e1183d41
Aluno <gustavooguidetti@gmail.com>
2020-03-10 09:48:47 -0300

    aula4

---
+++ aula4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Aula 4 - CSS e HTML</title>
+  <link rel="stylesheet" href="aula4.css">
+  <link href="https://fonts.googleapis.com/css?family=Bangers|Pacifico&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="site">
+    A
+
+    <div class="topo">
+      Topo do site
+
+      <div class="conteudo">
+        Corpo do site
+      </div>
+
+    <div class="rodape">
+      Desenvolvido por Guidaros
+    </div>
+
+    </div>
+
+  </div>
+
+</body>
+</html>
